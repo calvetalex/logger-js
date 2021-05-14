8acb283399dd5f70969d04eb0421bf4f1d9304f0
tic <calvet.alexandre@epitech.eu>
2021-05-14 16:23:46 +0200

    [ADD] first version of logger

---
+++ README.md
@@ -0,0 +1,38 @@
+# logger-js
+
+Free to use logger. It's a simple library that use console to print data and save it in files.
+
+## How to use
+
+As any other logger, you can use :
+
+```js
+log, warn, info, debug 
+```
+
+To use it in your project you can use it like:
+
+```js
+const logger = new Logger("onStdout"); // will write on your console
+const loggerWith = new Logger("WithLogfile", "./log"); // will write on your console and save in a logfile
+```
+
+The first argument is the name given to the process, the logfile will be called `my_app.log` in that case. Second parameter is the path to your log directory, if it's not given, the Logger will just write on stdout. If the path given is invalid, logfile will not be created and the Logger will just log on stdout.
+
+Furthermore, if an error occurred it will be logged in the standard logfile but also in a error logfile.
+
+## Logging
+
+Each method take a string or a variable as argument.
+
+If it's an object, it will be stringify before output to make it readable in a logfile. This is to avoid `[Object Object]` in the middle of your file. (WARNING : in JS `a = [1,2,3]` has an Object type : `typeof a` will give object)
+
+![log example](https://github.com/calvetalex/logger-js/blob/master/.images/output.png)
+
+## COMMENTS / ISSUES / PR
+
+Feel free to fork it and make your custom version
+
+## LICENSE
+
+MIT, see the source.
---
+++ logger-js.js
@@ -0,0 +1,104 @@
+// name logger-js.js
+// version: 1.0.0
+// https://github.com/calvetalex/logger-js
+/*
+
+Copyright (c) 2021 TIC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+
+const fs = require("fs");
+
+class Logger {
+  logfilePath = "";
+  errorPath = "";
+  standardStream;
+  errorStream;
+
+  constructor(name, path = "") {
+    if (!path || !fs.existsSync(path)) {
+      return;
+    }
+    this.logfilePath = `${
+      path.slice(-1) !== "/" ? path + "/" : path
+    }${name}-standard.log`;
+    this.errorPath = `${
+      path.slice(-1) !== "/" ? path + "/" : path
+    }${name}-error.log`;
+    this.standardStream = fs.createWriteStream(this.logfilePath, {
+      flags: "a+",
+    });
+    this.errorStream = fs.createWriteStream(this.errorPath, { flags: "a+" });
+  }
+
+  getDate() {
+    const date = new Date();
+    const yy = date.getFullYear();
+    const mm = String(date.getMonth() + 1).padStart(2, "0");
+    const dd = String(date.getDate()).padStart(2, "0");
+    const hh = String(date.getHours()).padStart(2, "0");
+    const mn = String(date.getMinutes()).padStart(2, "0");
+    return `${yy}-${mm}-${dd} . ${hh}:${mn}`;
+  }
+
+  message(content) {
+    return `[${this.getDate()}] ${content}`;
+  }
+
+  makeLog(type, toPrint) {
+    if (!toPrint) return;
+    let content = "";
+    if (typeof toPrint === "object" || toPrint instanceof Object) {
+      content = `${type}${this.message(JSON.stringify(toPrint))}`;
+    } else {
+      content = `${type}${this.message(toPrint)}`;
+    }
+    if (this.logfilePath) {
+      this.standardStream.write(content + "\n");
+      if (
+        type == "[ERR]" ||
+        content.match(/error/gi) ||
+        content.match(/erreur/gi)
+      ) {
+        this.errorStream.write(content + "\n");
+      }
+    }
+    return content;
+  }
+
+  log(val) {
+    console.log(this.makeLog("[LOG]", val));
+  }
+
+  info(val) {
+    console.info(this.makeLog("[INF]", val));
+  }
+
+  debug(val) {
+    console.debug(this.makeLog("[DEB]", val));
+  }
+
+  error(val) {
+    console.error(this.makeLog("[ERR]", val));
+  }
+}
+
+module.exports = Logger;
